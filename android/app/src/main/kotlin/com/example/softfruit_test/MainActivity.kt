f22932d2ae510ba221ec1ff1b0cd1e4cb6b823e3
nashva <nashvakizhakkayil@gmail.com>
2024-07-06 17:56:32 +0530

    initial commit

---
+++ android/app/src/main/kotlin/com/example/softfruit_test/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.softfruit_test
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ lib/main.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+import 'package:softfruit_test/pages/homepage.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'venue',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
+      ),
+      home: HomePage(),
+    );
+  }
+}
